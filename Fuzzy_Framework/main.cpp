4380194763addaf7b6a8146193efc2f95e5f1387
KVN <kvndesign.mail@gmail.com>
2013-03-13 11:06:46 +0100

    Test de AndMult

--- Fuzzy_Framework/main.cpp
+++ Fuzzy_Framework/main.cpp
@@ -1,18 +1,20 @@
 #include "core\ValueModel.h"
 #include "fuzzy\AndMin.h"
+#include "fuzzy\AndMult.h"
 #include <iostream>
 
 void main(){
 
-	fuzzy::AndMin<int> op;
+	fuzzy::AndMin<int> opMin;
+	fuzzy::AndMult<int> opMult;
 	core::ValueModel<int> v1(8);
 	core::ValueModel<int> v2(4);
 
 	std::cout << "premiere valeur: " << v1.evaluate() << std::endl;
-	std::cout << "deuxieme valeur: " << v2.evaluate() << std::endl;
+	std::cout << "deuxieme valeur: " << v2.evaluate() << std::endl << std::endl;
 
-	std::cout << "AndMin: " << op.evaluate(&v1, &v2) << std::endl;
-
+	std::cout << "AndMin: " << opMin.evaluate(&v1, &v2) << std::endl;
+	std::cout << "AndMult: " << opMult.evaluate(&v1, &v2) << std::endl;
 	std::cin.ignore();
 
 }
