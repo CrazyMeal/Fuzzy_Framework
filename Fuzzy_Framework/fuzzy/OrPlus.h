a79614a731549a64f6604a251838510bfb9d5d3f
KVN <kvndesign.mail@gmail.com>
2013-03-13 11:23:36 +0100

    Implementation de OrPlus

--- Fuzzy_Framework/fuzzy/OrPlus.h
+++ Fuzzy_Framework/fuzzy/OrPlus.h
@@ -1,13 +1,26 @@
 #ifndef ORPLUS_H
 #define ORPLUS_H
 
+#include "../core/Expression.h"
+#include "Or.h"
+
 namespace fuzzy
 {
 	template <class T>
 	class OrPlus : public Or<T>
 	{
 	public:
-		virtual T evaluate();
+		virtual T evaluate(core::Expression<T>*, core::Expression<T>*) const;
+	};
+
+	template <class T>
+	T OrPlus<T>::evaluate(core::Expression<T>* _left, core::Expression<T>* _right) const
+	{
+		T left = _left->evaluate();
+		T right = _right->evaluate();
+		T resultat = left+right;
+
+		return resultat;
 	}
 }
 
