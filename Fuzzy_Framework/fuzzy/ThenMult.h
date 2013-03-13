7ad6711ff0443399f5e306c5940586940f97795f
KVN <kvndesign.mail@gmail.com>
2013-03-13 11:57:19 +0100

    Creation et implementation de ThenMult
    Pareil que ThenMin sauf que le resultat est une multiplication

---
+++ Fuzzy_Framework/fuzzy/ThenMult.h
@@ -0,0 +1,26 @@
+#ifndef THENMULT_H
+#define THENMULT_H
+
+#include "../core/Expression.h"
+#include "Then.h"
+
+namespace fuzzy
+{
+	template <class T>
+	class ThenMult : public Then<T>
+	{
+	public:
+		virtual T evaluate(core::Expression<T>*, core::Expression<T>*) const;
+	};
+
+	template <class T>
+	T ThenMin<T>::evaluate(core::Expression<T>* _left, core::Expression<T>* _right) const
+	{
+		T left = _left->evaluate();
+		T right = _right->evaluate();
+		T resultat = left*right;
+
+		return resultat;
+	}
+}
+#endif
