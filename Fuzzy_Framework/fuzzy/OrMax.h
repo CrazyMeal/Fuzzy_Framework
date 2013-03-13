3fc1d31e27dbcf224667fa3047a97aee935f5b07
KVN <kvndesign.mail@gmail.com>
2013-03-13 11:32:27 +0100

    Implementation du evaluate de OrMax

--- Fuzzy_Framework/fuzzy/OrMax.h
+++ Fuzzy_Framework/fuzzy/OrMax.h
@@ -12,5 +12,14 @@ namespace fuzzy
 	public:
 		virtual T evaluate(core::Expression<T>*, core::Expression<T>*) const;
 	};
+
+	template <class T>
+	T OrMax<T>::evaluate(core::Expression<T>* _left, core::Expression<T>* _right) const
+	{
+		T left = _left->evaluate();
+		T right = _right->evaluate();
+
+		return (left>=right)? left : right ;
+	}
 }
 #endif
